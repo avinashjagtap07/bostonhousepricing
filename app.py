b302dd495bc1e7b2c9a1412ea504fee1a1848728
Avinash Jagtap <avinashjagtap07@gmail.com>
2023-06-26 15:35:42 +0530

    This commit includes requirements.txt and readme files

---
+++ app.py
@@ -0,0 +1,19 @@
+import pickle
+
+from flask import Flask, request, app, jsonify, url_for, render_template, redirect, flash, session, escape
+import numpy as np 
+import pandas as Pd
+
+app= Flask(__name__)
+## Load the model
+regmodel= pickle.load(open('regmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    
