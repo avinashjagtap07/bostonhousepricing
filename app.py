1ad830acf12c278dde5294cab21171a5a93c437f
Avinash Jagtap <avinashjagtap07@gmail.com>
2023-06-26 17:01:55 +0530

    This is commit for web application

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as Pd
 app= Flask(__name__)
 ## Load the model
 regmodel= pickle.load(open('regmodel.pkl','rb'))
+scaler= pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -16,4 +17,13 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    
+    print(np.array(list(data.values())).reshape(1,-1))
+
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
